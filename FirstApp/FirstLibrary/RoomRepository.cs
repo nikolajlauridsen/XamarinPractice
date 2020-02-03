b427c4a14b40efff6b7586ded10f45b067e81afb
Molex <simplesleeptimer@gmail.com>
2020-02-03 14:51:53 +0100

    Initial commit

---
+++ FirstApp/FirstLibrary/RoomRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstLibrary
+{
+    public class RoomRepository
+    {
+        private List<TrainingRoom> _rooms = new List<TrainingRoom>();
+
+        public RoomRepository()
+        {
+            _rooms.Add(new TrainingRoom
+            {
+                Id = 1,
+                Name = "Copernicus",
+                Location = "bldg 1",
+                NumberComputers = 25
+            });
+
+            _rooms.Add(new TrainingRoom
+            {
+                Id = 2,
+                Name = "Sagittarius",
+                Location = "bldg 1",
+                NumberComputers = 10
+            });
+
+            _rooms.Add(new TrainingRoom
+            {
+                Id = 3,
+                Name = "Luna",
+                Location = "bldg 3",
+                NumberComputers = 50
+            });
+        }
+
+        public List<TrainingRoom> GetRooms()
+        {
+            return _rooms;
+        }
+
+        public TrainingRoom GetRoom(int id)
+        {
+            return (from r in _rooms 
+                    where r.Id == id 
+                    select r).FirstOrDefault();
+        }
+    }
+}
---
+++ FirstApp/FirstLibrary/TrainingRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstLibrary
+{
+    public class TrainingRoom
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public int NumberComputers { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
