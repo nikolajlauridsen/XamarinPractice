0c220263ad186d02ea325091615b7981deb08155
Mole <nikolajlauridsen@protonmail.ch>
2020-02-04 14:24:32 +0100

    Add IOS implementation

---
+++ HPlusSports/HPlusSports/HPlusSports.iOS/NetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Configuration;
+using SystemConfiguration;
+using Foundation;
+using UIKit;
+
+[assembly:Xamarin.Forms.Dependency(typeof(HPlusSports.iOS.NetworkManager))]
+namespace HPlusSports.iOS
+{
+    public class NetworkManager : INetworkManager
+    {
+        public bool IsNetworkConnected()
+        {
+            NetworkReachabilityFlags flags;
+            IPAddress adress = new IPAddress(0);
+            NetworkReachability reachability = new NetworkReachability(adress);
+
+            if (reachability.TryGetFlags(out flags))
+            {
+                return (flags & NetworkReachabilityFlags.Reachable) != 0;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
