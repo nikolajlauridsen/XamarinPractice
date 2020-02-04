0693084dece41c163da86d0871794a289b759444
Mole <nikolajlauridsen@protonmail.ch>
2020-02-04 09:50:29 +0100

    Clean up main activity

--- FirstApp/TrainingRooms/MainActivity.cs
+++ FirstApp/TrainingRooms/MainActivity.cs
@@ -21,8 +21,6 @@ namespace TrainingRooms
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            //Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            //SetSupportActionBar(toolbar);
             RoomRepository repo = new RoomRepository();
 
             List<TrainingRoom> rooms = repo.GetRooms();
@@ -33,29 +31,6 @@ namespace TrainingRooms
             this.ListAdapter = adapter;
         }
 
-        //public override bool OnCreateOptionsMenu(IMenu menu)
-        //{
-        //    MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-        //    return true;
-        //}
-
-        //public override bool OnOptionsItemSelected(IMenuItem item)
-        //{
-        //    int id = item.ItemId;
-        //    if (id == Resource.Id.action_settings)
-        //    {
-        //        return true;
-        //    }
-
-        //    return base.OnOptionsItemSelected(item);
-        //}
-
-        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        //{
-        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        //}
 	}
 }
 
