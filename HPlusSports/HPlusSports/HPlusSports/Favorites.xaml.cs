a46706bf090da928737287b7d3e53a1a480892e2
Molex <simplesleeptimer@gmail.com>
2020-02-05 13:42:31 +0100

    Start working on favorites

---
+++ HPlusSports/HPlusSports/HPlusSports/Favorites.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HPlusSports.Services;
+
+namespace HPlusSports
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Favorites : ContentPage
+    {
+        public Favorites()
+        {
+            InitializeComponent();
+        }
+
+        public Favorites(List<Product> p)
+        {
+            BindingContext = p;
+        }
+
+        public void Item_Selected()
+        {
+
+        }
+    }
+}
