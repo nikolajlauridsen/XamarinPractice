be685a1f32647979f15c33c8a08af61222270d3f
Mole <nikolajlauridsen@protonmail.ch>
2020-02-10 12:02:23 +0100

    Add tuts stuff

---
+++ Paperboi/Paperboi/Paperboi/Pages/TestingPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Paperboi.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestingPage : ContentPage
+    {
+        public TestingPage()
+        {
+            InitializeComponent();
+        }
+
+        // Change UI with orientation change
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (Width > height)
+            {
+                outerStack.Orientation = StackOrientation.Horizontal;
+            }
+            else
+            {
+                outerStack.Orientation = StackOrientation.Vertical;
+            }
+        }
+    }
+}
